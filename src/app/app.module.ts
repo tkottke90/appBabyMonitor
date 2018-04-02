6dc62eaa5ee7613b8e59a8c3cac1e9581e8756d9
tkottke90 <altotom90@gmail.com>
2018-04-02 18:33:48 -0500

    Finished Setting Up Theme and Adding Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule
+} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { Browser } from 'protractor';
 
 
 @NgModule({
@@ -10,7 +17,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
